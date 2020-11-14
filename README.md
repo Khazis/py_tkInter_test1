40f88e31a747bf2b06b3054e389cccfbb1c60819
Khazis <khazis@wp.pl>
2020-11-14 09:27:09 +0100

    Tabelka z danymi

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 ## Moja droga do przygotowania interfejsu dla pierwszego programu w pythonie
 
 Planowane kroki
-+ napisanie hello world
-+ otwarcie pierwszego okienka
-+ dodanie przycisku ktory dla sie kliknac
-+ dodanie pola do wprowadzania danych
-+ organizacja elementow w okienku
-- stworzenie listy z wprowadzanymi danymi
-- utworzenie pola ktore bedzie dzialac jak konsola - mozna swobodnie wypisywac w nim tekst
-- stworzenie menu programu
-- wyswietlenie grafiki w okienku
+- [+] napisanie hello world
+- [+] otwarcie pierwszego okienka
+- [+] dodanie przycisku ktory dla sie kliknac
+- [+] dodanie pola do wprowadzania danych
+- [+] organizacja elementow w okienku
+- [+] przewijane pole 
+- [+] dane tabelaryczne
+- [] dane tabelaryczne z przewijaniem
+- [] stworzenie listy z wprowadzanymi danymi
+- [] utworzenie pola ktore bedzie dzialac jak konsola - mozna swobodnie wypisywac w nim tekst
+- [] stworzenie menu programu
+- [] wyswietlenie grafiki w okienku
--- another.py
+++ another.py
@@ -25,29 +25,52 @@ wrapper2.pack(fill="both", expand="yes", padx=10, pady=10)
 for i in range(50):
     Button(myframe, text="My Button - "+str(i)).pack()
 
-tv=ttk.Treeview(wrapper1, columns=(1,2,3,4,5,6,7,8), show="headings", height="5")
+tv=ttk.Treeview(wrapper1)
+tv["columns"]=(1,2,3,4,5,6,7,8)
+
+tv.heading("#0", text="Parent")
+tv.column("#0", minwidth=50, width=80, stretch=NO, anchor=W) 
+tv.heading(1, text="SSID")
+tv.column(1, minwidth=80, width=300, stretch=YES, anchor=W) 
+tv.heading(2, text="IP")
+tv.column(2, minwidth=100,  stretch=YES, anchor=W) 
+tv.heading(3, text="APC")
+tv.column(3, minwidth=50, width=80, stretch=NO, anchor=CENTER) 
+tv.heading(4, text="KANAŁ")
+tv.column(4, minwidth=50, width=80, stretch=NO, anchor=CENTER) 
+tv.heading(5, text="SZYBKOŚĆ")
+tv.column(5, minwidth=50, width=80, stretch=NO, anchor=CENTER) 
+tv.heading(6, text="PING")
+tv.column(6, minwidth=50, width=80, stretch=NO, anchor=CENTER) 
+tv.heading(7, text="JaMAX")
+tv.column(7, minwidth=50, width=80, stretch=NO, anchor=CENTER) 
+tv.heading(8, text="PaMAX")
+tv.column(8, minwidth=50, width=80, stretch=NO, anchor=CENTER) 
+
+dane=[
+    ["AP04","10.100.100.2","23","5400","9/78","65","23","35"],
+    ["AP01","10.100.100.2","34","5420","9/78","12","78","23"],
+    ["AP06","10.100.100.2","12","5280","9/78","34","34","12"],
+    ["AP34","10.100.100.2","53","5660","9/78","23","83","54"],
+    ["AP02","10.100.100.2","12","5180","9/78","76","35","12"]
+]
+
+nr=0
+for linia in dane:
+    tv.insert(parent="", index='end', iid=nr, text="Parent", values=(linia[0],linia[1],linia[2],linia[3],linia[4],linia[5],linia[6],linia[7]))    
+    nr+=1
+
+#tv.insert(parent="", index='end', iid=1, text="Parent", values=("AP04","10.100.100.2","23","5400","9/78","65","23","35"))
+#tv.insert(parent="", index='end', iid=0, text="Parent", values=("AP01","10.100.100.2","34","5420","9/78","12","78","23"))
+#tv.insert(parent="", index='end', iid=2, text="Parent", values=("AP06","10.100.100.2","12","5280","9/78","34","34","12"))
+##tv.insert(parent="", index='end', iid=3, text="Parent", values=("AP34","10.100.100.2","53","5660","9/78","23","83","54"))
+#tv.insert(parent="", index='end', iid=4, text="Parent", values=("AP02","10.100.100.2","12","5180","9/78","76","35","12"))
+
+tv.move("4","0","0")
 tv.pack(expand=YES, fill=BOTH)
 
-tv.heading(1, text="SSID")
-tv.column(1, minwidth=50, width=300, stretch=NO) 
-tv.heading(2, text="IP")
-tv.column(2, minwidth=50, width=200, stretch=NO) 
-tv.heading(3, text="APC")
-tv.column(3, minwidth=50, width=80, stretch=NO) 
-tv.heading(4, text="KANAŁ")
-tv.column(4, minwidth=50, width=80, stretch=NO) 
-tv.heading(5, text="SZYBKOŚĆ")
-tv.column(5, minwidth=50, width=80, stretch=NO) 
-tv.heading(6, text="PING")
-tv.column(6, minwidth=50, width=80, stretch=NO) 
-tv.heading(7, text="JaMAX")
-tv.column(7, minwidth=50, width=80, stretch=NO) 
-tv.heading(8, text="PaMAX")
-tv.column(8, minwidth=50, width=80, stretch=NO) 
-
-
-
 win.geometry("1100x500")
-win.resizable(False,False)
-win.title("Scroller")
+win.resizable(True,True)
+win.minsize(700,500)
+win.title("Nowa Fajna Apka")
 win.mainloop()
